31159df61dc20e680fa38781fb4bb89f87681be6
Stefan Els <stefanels1@gmail.com>
2020-10-31 23:28:38 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {MatSnackBar} from '@angular/material/snack-bar';
+import {FirebaseService} from './firebase.service';
+import {jsonFromFile} from './helper';
+import {CollectionData} from './models/collection-data';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'json-importer';
+  configText: string;
+  data: CollectionData[];
+
+  constructor(private snack: MatSnackBar, private service: FirebaseService) {
+  }
+
+  initFirebase(): void {
+    this.service.init(this.configText);
+  }
+
+  async handleData(event: any): Promise<void> {
+    const jsonData = await jsonFromFile(event.target.files[0] as File);
+
+    this.data = CollectionData.fromJSON(jsonData);
+  }
+
+  upload(): void {
+    this.service.upload(this.data);
+  }
+
+  cleanText(text: string): string {
+    return text
+      .split('\n')
+      .map(line => line
+        .replace('" ', '')
+        .replace('",', '')
+        .replace('"', '')
+        .trim()
+      ).join('\n');
+  }
+
+  handleConfig(): void {
+    console.log("Config");
+    this.configText = this.cleanText(this.configText);
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MaterialFileInputModule} from 'ngx-material-file-input';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {FormsModule} from '@angular/forms';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FlexLayoutModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    MaterialFileInputModule,
+    MatIconModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
